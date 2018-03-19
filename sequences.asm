075e75b03a8e11bb2a173b4c3f4390b4316db433
Piotr Szulc <kkragoth@gmail.com>
2018-03-19 13:07:51 +0000

    asm master

--- sequences.asm
+++ sequences.asm
@@ -1,15 +1,16 @@
+%define ARR_LEN 256
 %define BUFFER_SIZE 2048
 
-%define SYS_READ 3
-%define SYS_OPEN 5
-%define SYS_CLOSE 6
+%define SYS_READ 0
+%define SYS_OPEN 2
+%define SYS_CLOSE 3
 %define SYS_EXIT 60
 
 %define READONLY 0
 
 section .data
-    P: times 256 db 0
-    O: times 256 db 0
+    P: times ARR_LEN db 0
+    O: times ARR_LEN db 0
 
 section .bss
     input_value: resb 1
@@ -19,7 +20,7 @@ section .bss
 ; ebx - file descriptor
 ; ecx - Getting_value_from_array
 ; r10 - is_first_permutation
-; r11 - numbers_in_current_set
+; r15 - numbers_in_current_set
 ; r12 - numbers_in_first_set
 ; r13 - is_valid
 
@@ -28,33 +29,34 @@ global _start
 _start:
     nop
 
-    pop rbx ;number of command line parameters
-    cmp rbx, 2 ; check if we get file parameter
+	mov eax, 1;
+	mov bl, [P + eax]
+
+    pop rdi ;number of command line parameters
+    cmp rdi, 2 ; check if we get file parameter
     jne .exit_not_ok
 
-    pop rbx ;name of the program - will be disregarded
+    pop rdi ;name of the program - will be disregarded
 
-    pop rbx ;name of the file to be opened
-    cmp ebx, 0 ;check if ebx is ok
-    jbe .exit_not_ok
+    pop rdi ;name of the file to be opened
 
     ;open file in read mode
-    mov eax, SYS_OPEN
-    mov ecx, READONLY
+    mov rax, SYS_OPEN
+    mov rsi, READONLY
     syscall
     
-    cmp eax, 0 ;check if fd > 0 (ok)
-    jbe .exit_not_ok
-    mov ebx, eax
+    cmp rax, 0 ;check if fd > 0 (ok)
+    jle .exit_not_ok
+    mov rdi, rax
 
     mov r10, 1
-    xor r11, r11 ;numbers_in_current_permutation = 0
+    xor r15, r15 ;numbers_in_current_permutation = 0
     xor r12, r12 ;numbers_in_first_permutation = 0
     xor r13, r13 ;is_valid = 0;
 .main_loop:
-    mov eax, SYS_READ
-    mov ecx, input_value
-    mov edx, 1 ;read one byte
+    mov rax, SYS_READ
+    mov rsi, input_value
+    mov rdx, 1 ;read one byte
 
     syscall
     js .exit_not_ok ;file is open but cannot be read
@@ -62,39 +64,40 @@ _start:
     cmp eax, 1;check number of bytes read
     jb .ending_protocol
 
-    mov al, [input_value]
+    mov rax, [input_value]
 
     cmp al, 0
     je .handle_zero
 
     ;non_zero
+	xor r13, r13
     cmp r10, 0
     je .handle_non_zero_in_non_first_permutation
 .handle_non_zero_in_first_permutation:
-    mov cl, [O + eax]
-    cmp cl, 1
+    mov dl, [O + eax]
+    cmp dl, 1
     je .exit_not_ok
 
-    inc cl
-    mov [O + eax], cl
+    inc dl
+    mov [O + eax], dl
     inc r12
     jmp .main_loop
     
 .handle_non_zero_in_non_first_permutation:
-    mov cl, [P + eax]
-    cmp cl, 0
+    mov dl, [P + eax]
+    cmp dl, 0
     je .exit_not_ok
 
-    dec r11
-    dec cl
-    mov [P + eax], cl
+    dec r15
+    dec dl
+    mov [P + eax], dl
 
 
     jmp .main_loop
 
 .handle_zero:
     xor r10, r10; first_permutation = 0;
-    cmp r11, 0
+    cmp r15, 0
     je .valid
     xor r13, r13
     jmp .next
@@ -102,7 +105,7 @@ _start:
 .valid:
     mov r13, 1
 .next:
-    mov r11, r12; numbers_in_current_permutation = 0;
+    mov r15, r12; numbers_in_current_permutation = 0;
     
     mov r14, [O]
     mov [P], r14
